6b6ee213fc17014aee106fdfb44884115e274930
Yasin Yakup Taşpınar <yyt1.618@gmail.com>
2024-06-27 17:29:40 +0300

    calculate_bmi added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 my_window = Tk()
 my_window.title("Vücut Kitle İndeksi Hesaplayıcı")
-my_window.minsize(width=300, height=300)
+my_window.minsize(width=300, height=250)
 
 my_weight_label = Label(text="Ağırlığınızı giriniz (kg)")
 my_weight_label.config(padx=10, pady=10)
@@ -18,7 +18,29 @@ my_height_label.pack()
 my_height_entry = Entry(width=20)
 my_height_entry.pack()
 
-my_calculate_button = Button(text="Hesapla")
+def calculate_bmi():
+    try:
+        weight = float(my_weight_entry.get())
+        height = float(my_height_entry.get()) / 100
+        if 500 > weight > 0 and 500 > height > 0:
+            bmi = weight / (height ** 2)
+            if bmi < 18.5:
+                my_result_label.config(text=f"Vücut kitle indeksiniz {bmi:.2f} olup zayıfsınız." )
+            elif 25 > bmi >= 18.5:
+                my_result_label.config(text=f"Vücut kitle indeksiniz {bmi:.2f} olup normal kilodasınız.")
+            elif 30 > bmi >= 25:
+                my_result_label.config(text=f"Vücut kitle indeksiniz {bmi:.2f} olup fazla kilolusunuz.")
+            elif bmi >= 30:
+                my_result_label.config(text=f"Vücut kitle indeksiniz {bmi:.2f} olup obezsiniz.")
+        else:
+            my_result_label.config(text="Lütfen geçerli bir değer giriniz")
+    except ValueError:
+        my_result_label.config(text="Lütfen geçerli bir değer giriniz")
+
+my_calculate_button = Button(text="Hesapla", command=calculate_bmi)
 my_calculate_button.pack(pady=20)
+
+my_result_label = Label(text="")
+my_result_label.pack()
 
 my_window.mainloop()
